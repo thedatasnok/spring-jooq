d75bc79309bad019b402e4944b26acc45053e25f
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-02-16 18:41:54 +0100

    refactor(migrations): make liquibase bean depend on schema creation

--- packages/migrations/src/main/java/cool/datasnok/samples/springjooq/migrations/MigrationsApp.java
+++ packages/migrations/src/main/java/cool/datasnok/samples/springjooq/migrations/MigrationsApp.java
@@ -5,9 +5,11 @@ import javax.sql.DataSource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import jakarta.annotation.PostConstruct;
+import liquibase.integration.spring.SpringLiquibase;
 
 @SpringBootApplication
 public class MigrationsApp {
@@ -21,9 +23,19 @@ public class MigrationsApp {
     SpringApplication.run(MigrationsApp.class, args);
   }
 
-  @PostConstruct
-  void init() {
+  @Bean
+  boolean schemaCreation() {
     new JdbcTemplate(this.dataSource).execute("CREATE SCHEMA IF NOT EXISTS library");
+    return true; // assuming the above call will throw and cause an error before returning if it fails
+  }
+
+  @Bean
+  @DependsOn("schemaCreation")
+  SpringLiquibase liquibase() {
+    var liquibase = new SpringLiquibase();
+    liquibase.setChangeLog("classpath:db/changelog/db.changelog-master.yaml");
+    liquibase.setDataSource(this.dataSource);
+    return liquibase;
   }
 
 }
